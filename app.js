0be5495894e8e8722c8d2a3727f4106efcd2911d
Syed Mubashir Ali <smubashirali620@mail.com>
2025-04-21 15:58:32 +0500

    added mail service, env and db configs and app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+require("dotenv").config();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
+
+const PORT = process.env.PORT || 3000;
+const ENV = process.env.NODE_ENV || "development";
+
+//routes
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send({
+      message: "Hello there, this is a Node js Trip Sync API.",
+      author: "Syed Mubashir Ali",
+      server_alive: true,
+    });
+});
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`=====================================`);
+  console.log(`Launching Trip Sync backend Application`);
+  console.log(
+    `🚀 Application listening on Port ${PORT} on the ${ENV} Environment`
+  );
+  console.log(
+    `Date: ${new Date(Date.now()).toLocaleDateString()} Time: ${new Date(
+      Date.now()
+    ).toLocaleTimeString()}`
+  );
+  console.log(`=====================================`);
+});
---
+++ src/services/mail.service.js
@@ -0,0 +1,30 @@
+// mailer.js
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail', 
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+async function sendEmail(to, subject, html) {
+  const mailOptions = {
+    from: `"Trip Sync" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent:', info.response);
+    return info;
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error;
+  }
+}
+
+module.exports = sendEmail;
