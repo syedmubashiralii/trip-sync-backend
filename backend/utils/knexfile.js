ca5f6201f32a144e25a8e70ab418baf95a950f27
Syed Mubashir Ali <smubashirali620@mail.com>
2025-06-03 19:27:00 +0500

    added pass in prod env

--- backend/utils/knexfile.js
+++ backend/utils/knexfile.js
@@ -31,11 +31,12 @@ module.exports = {
     },
   },
   production: {
-    client: "mysql",
+    client: "postgresql",
     connection: {
       host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
